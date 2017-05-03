2191e2aad813e955c2fd8e2e729c72fafc6ce238
Siang-Siang Huang <siahuang@umich.edu>
2017-05-03 11:24:43 -0400

    file upload

---
+++ js/jvascript.js
@@ -0,0 +1,8 @@
+function petDog() {
+	if(document.getElementById("dogN").src == "../MDAT2/img/dog-normal.png") {
+		document.getElementById("dogN").src = "../MDAT2/img/dog-long.png"
+	}
+	else {
+		document.getElementById("dogN").src = "../MDAT2/img/dog-normal.png"
+	}
+}
